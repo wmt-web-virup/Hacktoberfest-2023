22a8c25388140c69567ce6ae99d710fc0e43d924
Kalyani Modi <kalyanim@webmobtech.com>
2023-10-13 12:23:30 +0530

    Added code 3

---
+++ Kalyani Modi/code3.js
@@ -0,0 +1,18 @@
+function decimalToBinary(decimal) {
+    if (decimal === 0) {
+      return '0';
+    }
+  
+    let binary = '';
+    while (decimal > 0) {
+      binary = (decimal % 2) + binary;
+      decimal = Math.floor(decimal / 2);
+    }
+  
+    return binary;
+  }
+  
+  const decimalNumber = 42; // Replace with your desired decimal number
+  const binaryRepresentation = decimalToBinary(decimalNumber);
+  console.log(`Binary representation of ${decimalNumber} is: ${binaryRepresentation}`);
+  
