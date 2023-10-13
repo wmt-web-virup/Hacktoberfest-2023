52a6f916ff208f0f265323384c10edc730915265
rajc1 <rajc@webmobtech.com>
2023-10-13 12:30:05 +0530

    solved reverse each word

---
+++ Raj Chokshi/Question 2/reverse_each_word_in_sentence.dart
@@ -0,0 +1,10 @@
+//Given a string, reverse each word in the sentence.
+
+void main(){
+  largestNumber("hello world");
+}
+void largestNumber(String str){
+
+  print(str.split('').reversed.join());
+}
+
